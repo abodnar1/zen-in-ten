afe9042619a93d5343b6f1ced008b5885c097862
Amber Bodnar <amber.bodnar@gmail.com>
2022-07-23 17:23:12 -0400

    Add component folders and removed unnecessary files

---
+++ src/QuoteCard/QuoteCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const QuoteCard = () => {
+  return (
+    <div></div>
+  )
+};
+
+export default QuoteCard;
---
+++ src/Quotes/Quotes.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Quotes = () => {
+  return (
+    <div></div>
+  )
+};
+
+export default Quotes;
