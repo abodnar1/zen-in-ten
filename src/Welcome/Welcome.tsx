58ab10b819f37108690a36a121bdcf472a827895
Amber Bodnar <amber.bodnar@gmail.com>
2022-07-23 17:59:08 -0400

    Add classes to Welcome component elements

--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -3,7 +3,11 @@ import "./Welcome.css";
 
 const Welcome = () => {
   return (
-    <div className="welcome-container">WELCOME</div>
+    <div className="welcome-container">
+      <h1 className="welcome-header">Welcome!</h1>
+      <p className="welcome-message">blurb about site</p>
+      <button className="get-started-button">Get Started!</button>
+    </div>
   )
 };
 
