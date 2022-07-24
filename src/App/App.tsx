082a5174b7bdd72fab93bbb8844ff7ff0e411e40
“Corinne <corinne.canavan@gmail.com>
2022-07-24 12:06:58 -0600

    Add catch back into fetch, update App with useState for error

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,24 +6,27 @@ import { Route, Switch } from "react-router-dom";
 interface Quote {
   q: string;
   a: string;
+  h: string;
 }
 
 const App = () => {
   const [allQuotes, setAllQuotes] = useState<Quote[]>([])
+  const [error, setError] = useState<string>('')
 
   useEffect(() => {
     fetch('https://zenquotes.io/api/random?key=11ef57ae8191dde524535934c158c4543950e06c')
     .then(response => response.json())
     .then(data => setAllQuotes(data))
-    .catch(error => console.log(error.message))
-  }, []);
+    .catch(error => setError(error.message))
+  },[]);
+
 
   return (
-    <Switch>
-      <Route exact path="/" >
-        <Welcome />
-      </Route>
-    </Switch>
+    <div>
+      <h1>Hello World</h1>
+        {error && <p>{error}</p>}
+        {allQuotes.length > 0 && <p>{allQuotes[0].q}</p>}
+    </div>
   );
 }
 
@@ -33,3 +36,14 @@ export default App;
 // Welcome = "/"
 // App = "/home"
 // Favorites = "/favorites"
+
+
+
+    /* <Switch>
+      <Route exact path="/" >
+        <Welcome />
+      </Route>
+      <Route path="/home" >
+      </Route>
+    </Switch> */
+
