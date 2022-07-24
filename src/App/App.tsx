c168ec0b6382890871826306461117e0d18bf58e
“Corinne <corinne.canavan@gmail.com>
2022-07-24 10:35:31 -0600

    Add useState and interface to App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Welcome from "../Welcome/Welcome";
 import { Route, Switch } from "react-router-dom";
 
+interface Quote {
+  q: string;
+  a: string;
+}
+
 const App = () => {
+  const [allQuotes, setAllQuotes] = useState<Quote[]>([])
+
   return (
     <Switch>
       <Route exact path="/" >
