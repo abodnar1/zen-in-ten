7ae0f89c95a961eca2226436174382a42a80ff95
“Corinne <corinne.canavan@gmail.com>
2022-07-26 10:59:03 -0600

    Add initial test for homepage

--- cypress/e2e/home_spec.cy.ts
+++ cypress/e2e/home_spec.cy.ts
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/home')
   })
+
+  it('should have a nav bar with links to Favorites page', () => {
+    cy.get('nav').contains('Favorites').click()
+    cy.url().should('eq', 'http://localhost:3000/favorites')
+  })
+
 })
