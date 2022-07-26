a2cf146399b2b9e4d1072f7d19110d94f0527726
“Corinne <corinne.canavan@gmail.com>
2022-07-26 11:03:57 -0600

    Add test for nav bar links

--- cypress/e2e/home_spec.cy.ts
+++ cypress/e2e/home_spec.cy.ts
@@ -1,11 +1,22 @@
+import { createYield } from "typescript"
+
 describe('Homepage', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/home')
   })
 
-  it('should have a nav bar with links to Favorites page', () => {
+  it('should have a nav bar with link to Favorites page', () => {
     cy.get('nav').contains('Favorites').click()
     cy.url().should('eq', 'http://localhost:3000/favorites')
+  })
+
+  it('should have a nav bar with link back to Home page', () => {
+    cy.get('nav').contains('Home').click()
+    cy.url().should('eq', 'http://localhost:3000/home')
+  })
+
+  it('should have a dropdown menu to select mood', () => {
+    
   })
 
 })
