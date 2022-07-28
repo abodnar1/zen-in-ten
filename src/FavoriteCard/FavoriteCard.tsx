9fb85972561bdc5bb7daa548c3ffc361dbf77ee6
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-28 15:27:46 -0600

    Add styling to favorites card

--- src/FavoriteCard/FavoriteCard.tsx
+++ src/FavoriteCard/FavoriteCard.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { QuoteInterface } from '../interfaces';
 import "./FavoriteCard.css"
+// import DeleteIcon from 'public/assets/delete-icon.png'
+import {AiOutlineDelete} from "react-icons/ai"
 
 interface FavoriteCardProps{
   quote: string,
   author: string,
+	image: string,
   setFavList: React.Dispatch<React.SetStateAction<QuoteInterface[]>>;
 	favList: QuoteInterface[];
   id: string
 }
 
-const FavoriteCard = ({quote, author, setFavList, favList, id}: FavoriteCardProps) => {
+const FavoriteCard = ({quote, author, image, setFavList, favList, id}: FavoriteCardProps) => {
   const deleteFavorite = (e: any) => {
     e.preventDefault()
     const favQuotes = favList.filter(favQuote => favQuote.q !== quote)
@@ -20,11 +23,14 @@ const FavoriteCard = ({quote, author, setFavList, favList, id}: FavoriteCardProp
   return (
     <div className="favorite-card-wrapper">
       <div className="top-container">
-        <h2>"{quote}"</h2>
-        <p>-{author}</p>
+        <h2 className="quote">"{quote}"</h2>
+        <p className="author">-{author}</p>
       </div>
+			<div className="middle-container">
+				<img src={image} className="quote-image"></img>
+			</div>
       <div className="bottom-container">
-        <button className="delete-button" onClick={(e) => deleteFavorite(e)}>Delete</button>
+				<button className="delete-button" onClick={(e) => deleteFavorite(e)}><AiOutlineDelete /></button>
       </div>
     </div>
   )
