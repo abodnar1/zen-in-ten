56abc27990ef70eed4e10ef2a296240b4cb91bc4
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-07-27 18:21:13 -0600

    Add styling to welcome page

--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -5,8 +5,10 @@ import "./Welcome.css";
 const Welcome = () => {
   return (
     <div className="welcome-container">
-      <h1 className="welcome-header">Welcome</h1>
-      <p className="welcome-message">blurb about site</p>
+      <h1 className="welcome-header">Welcome to<br></br><span className="zen-in-ten">Zen in Ten</span></h1>
+      <p className="welcome-message">Our goal at Zen in Ten is for you to find inspiration among our quote collection.<br></br>
+				After you get started, simply select a mood to find famous quotes to help ground you.<br></br>
+				YOU'RE ABOUT TO BE ZEN IN TEN.</p>
       <Link to="/home">
         <button className="get-started-button">Get Started!</button>
       </Link>
