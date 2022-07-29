63444ca716177cc19d0b0bf07dab7a0ab87a9681
“Corinne <corinne.canavan@gmail.com>
2022-07-29 14:11:59 -0600

    Add initial tests for favorites page when no quotes are favorited

--- cypress/e2e/favorites_spec.cy.ts
+++ cypress/e2e/favorites_spec.cy.ts
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Favorites page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/favorites')
+  })
+
+  it('should indicate user is on Favorites page in nav bar and header', () => {
+    cy.get('.nav-bar').find('.favorites-nav').should('have.class', 'active')
+    cy.contains('h2', 'Favorites')
+  })
+
+  it('should display a message if user has no quotes favorited', () => {
+    cy.get('.favorites-container').should('have.length', 0)
+    cy.contains('h3', 'No favorites yet!')
   })
 })
