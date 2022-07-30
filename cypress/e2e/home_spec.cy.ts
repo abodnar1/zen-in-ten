6d0d9b097704a1d4b96b2fd6dbba0e88cf3f45d4
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-07-29 21:14:57 -0700

    break up  home tests

--- cypress/e2e/home_spec.cy.ts
+++ cypress/e2e/home_spec.cy.ts
@@ -14,19 +14,20 @@ describe('Homepage', () => {
     cy.url().should('eq', 'http://localhost:3000/home')
   })
 
-  it('should have a dropdown menu to select mood and display appropriate cards; user should be able to click heart icon on card to save it to favorites page; user should be able to delete quote card from Favorites by clicking trash can icon', () => {
-   cy.get('form').find('select').select('fairness')
-   cy.get('.quotes-container').find('.card-wrapper').should('have.length', 2)
-
-   cy.get('.card-wrapper').eq(0).contains('h2', 'Everything that is made beautiful and fair and lovely is made for the eye of one who sees.')
-   cy.get('.card-wrapper').eq(0).contains('p', 'Rumi')
-
-   cy.get('.card-wrapper').eq(1).contains('h2', 'It is not fair to ask of others what you are not willing to do yourself.')
-   cy.get('.card-wrapper').eq(1).contains('p', 'Eleanor Roosevelt')
-
-   cy.get('.favorite-button').first().click()
-
-   cy.get('.nav-bar').find('.favorites-nav').click()
-   cy.url().should('eq', 'http://localhost:3000/favorites')
+  it("should have a favorites button", () => {
+    cy.get('.nav-bar').find('.favorites-nav').click()
+    cy.url().should('eq', 'http://localhost:3000/favorites')
   })
+  
+  it('should have a dropdown menu to select mood and display appropriate cards; user should be able to click heart icon on card to save it to favorites page; user should be able to delete quote card from Favorites by clicking trash can icon', () => {
+    cy.get('form').find('select').select('fairness')
+    cy.get('.quotes-container').find('.card-wrapper').should('have.length', 2)
+
+    cy.get('.card-wrapper').eq(0).contains('h2', 'Everything that is made beautiful and fair and lovely is made for the eye of one who sees.')
+    cy.get('.card-wrapper').eq(0).contains('p', 'Rumi')
+
+    cy.get('.card-wrapper').eq(1).contains('h2', 'It is not fair to ask of others what you are not willing to do yourself.')
+    cy.get('.card-wrapper').eq(1).contains('p', 'Eleanor Roosevelt')
+  })
+
 })
