f0148cc9cafab407cb552e84a0d4a16d143f4102
Amber Bodnar <amber.bodnar@gmail.com>
2022-07-30 14:43:40 -0400

    Change indentation in Favorites component

--- src/Components/Favorites/Favorites.tsx
+++ src/Components/Favorites/Favorites.tsx
@@ -5,19 +5,20 @@ import FavoriteCard from "../../Components/FavoriteCard/FavoriteCard";
 
 const Favorites = ({setFavList, favList}: FavoriteProps) => {
 
-    const favQuotes = favList.map((fav, index) => {
-      return (
-        <FavoriteCard
-          quote={fav.q}
-          author={fav.a}
-					image={fav.i}
-          key={index}
-          id={fav.q}
-          setFavList={setFavList}
-          favList={favList}
-        />
-      )
-    })
+  const favQuotes = favList.map((fav, index) => {
+    return (
+      <FavoriteCard
+        quote={fav.q}
+        author={fav.a}
+        image={fav.i}
+        key={index}
+        id={fav.q}
+        setFavList={setFavList}
+        favList={favList}
+      />
+    )
+  })
+  
   return ( 
     <div className="favorites-container">
       {favList.length ? favQuotes : <h3 className="fave-msg">No favorites yet! Click on the heart to favorite a quote!</h3>}
