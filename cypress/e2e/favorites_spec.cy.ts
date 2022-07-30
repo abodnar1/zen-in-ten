a9c29f683667dc75b285ec315679e475674ea077
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-07-29 21:00:53 -0700

    favorites testing sad path

--- cypress/e2e/favorites_spec.cy.ts
+++ cypress/e2e/favorites_spec.cy.ts
@@ -1,15 +1,28 @@
-// describe('Favorites page', () => {
+describe('Favorites page with no quotes', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/favorites')
+  })
+
+  it('should indicate user is on Favorites page in nav bar and header', () => {
+    cy.get('.nav-bar').find('.favorites-nav').should('have.class', 'active')
+    cy.contains('h2', 'Favorites')
+  })
+
+  it('should display a message if user has no quotes favorited', () => {
+    cy.get('.favorites-container').should('have.length', 0)
+    cy.contains('h3', 'No favorites yet!')
+  })
+})
+
+// describe('Favorites page with quotes', () => {
 //   beforeEach(() => {
-//     cy.visit('http://localhost:3000/favorites')
+//     cy.request('http://localhost:3000/favorites')
 //   })
 
-//   it('should indicate user is on Favorites page in nav bar and header', () => {
-//     cy.get('.nav-bar').find('.favorites-nav').should('have.class', 'active')
-//     cy.contains('h2', 'Favorites')
-//   })
+//   it("should", () => {
 
-//   it('should display a message if user has no quotes favorited', () => {
-//     cy.get('.favorites-container').should('have.length', 0)
-//     cy.contains('h3', 'No favorites yet!')
+//    cy.get('.favorite-card-wrapper').eq(0).contains('h2', 'Everything that is made beautiful and fair and lovely is made for the eye of one who sees.')
+//    cy.get('.favorite-card-wrapper').eq(0).contains('p', 'Rumi')
+
 //   })
 // })
