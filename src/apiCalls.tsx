2b5de7c1c777ee83d8d7b44ad29037040f027adb
Amber Bodnar <amber.bodnar@gmail.com>
2023-01-04 12:52:18 -0500

    Register new account for api

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,7 +1,7 @@
 import { Mood, QuoteInterface, DailyQuoteInterface } from "./interfaces"
 
 export const fetchAllQuoteKeywords = (): Promise<Mood[]> => {
-  return fetch("https://zenquotes.io/api/keywords/11ef57ae8191dde524535934c158c4543950e06c")
+  return fetch(`https://zenquotes.io/api/keywords/${process.env.REACT_APP_API_KEY}`)
   .then(response => response.json())
   .then(response => {
     return response as Mood[]
@@ -17,7 +17,7 @@ export const fetchQuotesByKeyword = (keywordValue: string): Promise<QuoteInterfa
 }
 
 export const fetchDailyQuote = (): Promise<DailyQuoteInterface[]> => {
-  return fetch("https://zenquotes.io/api/today/11ef57ae8191dde524535934c158c4543950e06c")
+  return fetch(`https://zenquotes.io/api/today/${process.env.REACT_APP_API_KEY}`)
   .then(response => response.json())
   .then(response => {
     return response as DailyQuoteInterface[]
